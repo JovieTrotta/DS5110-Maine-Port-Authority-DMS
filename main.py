16bb8edca2a7bdfac3cfe589e2b15a206e8a1742
JovieTrotta <jovietrotta@gmail.com>
2025-11-29 11:31:33 -0500

    added extraction and metadata to main function

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 # IMPORTS
+from extraction.extractwsubfolders import *
+from testing_code.extract_metadata import main as extract_meta_main
 
+pdf_input_dir = "pdf_files_up"
+docx_input_dir = "docx_files_up"
 
 # MAIN
 def main():
+
+    extract_meta_main(pdf_input_dir)
+    extract_meta_main(docx_input_dir)
 
     print("hello world")
 
