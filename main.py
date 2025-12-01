b634d131a1306fa6341eb796947941bb3e312ab5
JovieTrotta <jovietrotta@gmail.com>
2025-11-30 21:22:31 -0500

    updated folder_org name and main function - runs through whole program

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # IMPORTS
 from extraction.extractwsubfolders import *
 from testing_code.extract_metadata import main as extract_meta_main
+from document_title.documenttitling2 import main as title_files_main
+from folder_organization.organizer2 import *
 
 pdf_input_dir = "pdf_files_up"
 docx_input_dir = "docx_files_up"
@@ -10,6 +12,8 @@ def main():
 
     extract_meta_main(pdf_input_dir)
     extract_meta_main(docx_input_dir)
+
+    title_files_main(docx_input_dir, pdf_input_dir)
 
     print("hello world")
 
